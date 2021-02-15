a5d9914704cae9c19093fbe39891d15de6b420f4
maambo <78373393+maambo@users.noreply.github.com>
2021-02-15 11:55:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ import ReactDom from "react-dom"
 import { ModuleFilenameHelpers } from "webpack";
 
 ReactDom.render(<h1>This is my project</h1>,document.getElementById("root"))
+
+function myinfo(){
+  return (
+    <div> <h1> students </h1>
+         <p>learning React with jsx</p>
+         <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>java script</li>
+         </ul>
+    </div>
+  )  
+}
+ReactDom.render(myinfo,document .getElementById("root"))
