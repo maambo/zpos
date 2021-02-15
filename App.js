afaafb73ecaf3464211d97fb6ddac730e8add094
maambo <78373393+maambo@users.noreply.github.com>
2021-02-15 12:34:32 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+function App {
+    return(
+        <div>
+            <footer>
+                <p>This my footer</p>
+            </footer>
+        </div>
+    )
+}
+export default App;
