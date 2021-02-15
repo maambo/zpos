b177b27c1fe939eaee0c18c51d28b949060441d6
maambo <78373393+maambo@users.noreply.github.com>
2021-02-15 11:35:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom"
+import { ModuleFilenameHelpers } from "webpack";
+
+ReactDom.render(<h1>This is my project</h1>,document.getElementById("root"))
